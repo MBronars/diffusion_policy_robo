150a34b315953cc00dc626a112df5b7c1d392ebb
Bronars <mattbronars82@gmail.com>
2023-09-11 03:19:51 -0400

    fixed bash scripts and input dim

--- base_param_scan_trans.sh
+++ base_param_scan_trans.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the folder path
-folder="/srv/rl2-lab/flash8/mbronars3/workspace/diffusion_policy/config/ICRA/2block_base_param_trans"
+folder="/srv/rl2-lab/flash8/mbronars3/workspace/diffusion_policy_robo/diffusion_policy/config/ICRA/2block_base_param_trans"
 
 # Set the base command
-base_command="python train.py --config-dir /srv/rl2-lab/flash8/mbronars3/workspace/diffusion_policy/config/ICRA/2block_base_param_trans hydra.run.dir='/srv/rl2-lab/flash8/mbronars3/ICRA/results/hyperparam_scan/2block_base_params/\${now:%Y.%m.%d}/\${now:%H.%M.%S}_\${name}_\${task_name}'"
+base_command="python train.py --config-dir /srv/rl2-lab/flash8/mbronars3/workspace/diffusion_policy_robo/diffusion_policy/config/ICRA/2block_base_param_trans hydra.run.dir='/srv/rl2-lab/flash8/mbronars3/ICRA/results/hyperparam_scan/2block_base_params/\${now:%Y.%m.%d}/\${now:%H.%M.%S}_\${name}_\${task_name}'"
 
 # Loop through files in the folder and execute the base command with --config-name
 for file in "$folder"/*; do
